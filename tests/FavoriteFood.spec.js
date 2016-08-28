133cfe55d4dc2c9974bafdd093f34e05e0234809
Savannah Scott <sascott624@gmail.com>
2016-08-28 19:42:36 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,23 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	it('should correctly filter favorite foods', function(){
+		var pickyEaters = [{
+			name: "Molly",
+			favoriteFood: "lemonade"
+		}, {
+			name: "Brad",
+			favoriteFood: "cookies"
+		}, {
+			name: "Krista",
+			favoriteFood: "kale"
+		}];
+
+		var results = $filter('favoriteFood')(pickyEaters, 'lemonade');
+
+		expect(results.length).toBe(1);
+		expect(results[0].name).toBe('Molly');
+	})
+
+
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,12 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	it('should remove all vowels from a string', function(){
+		var results = $filter('removeAllVowels')('lemonade');
+		expect(results).toEqual('lmnd');
+
+		var sentence = $filter('removeAllVowels')('this is a sentence');
+		expect(sentence).toEqual('ths s  sntnc');
+	})
+
 });
